cdd5f237cc498010ec69d2a9cabbe3e1dda296bc
sam253narula <samarthnarula253@gmail.com>
2021-11-21 17:02:21 +0530

    refactor produer class name

--- spring-kafka-producer-example/src/main/java/com/example/KafkaProducerApplication.java
+++ spring-kafka-producer-example/src/main/java/com/example/KafkaProducerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringKafkaExampleApplication {
+public class KafkaProducerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringKafkaExampleApplication.class, args);
+		SpringApplication.run(KafkaProducerApplication.class, args);
 	}
 
 }
