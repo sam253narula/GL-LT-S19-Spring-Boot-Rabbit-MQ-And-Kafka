66d514bf99a87b0a05a0531ec9db8880c2c83f8c
sam253narula <samarthnarula253@gmail.com>
2021-11-21 17:00:03 +0530

    initial commit

---
+++ spring-kafka-consumer-example/src/main/java/com/example/model/Employee.java
@@ -0,0 +1,14 @@
+package com.example.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Employee {
+
+	private Integer id;
+	private String name;
+}
---
+++ springboot-rabbitmq-example/src/main/java/com/springboot/rabbitmq/publisher/OrderPublisher.java
@@ -0,0 +1,27 @@
+package com.springboot.rabbitmq.publisher;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.springboot.rabbitmq.config.MessagingConfig;
+import com.springboot.rabbitmq.dto.Order;
+import com.springboot.rabbitmq.dto.OrderStatus;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/order")
+public class OrderPublisher {
+
+    @Autowired
+    private RabbitTemplate template;
+
+    @PostMapping("/{restaurantName}")
+    public String bookOrder(@RequestBody Order order, @PathVariable String restaurantName) {
+        order.setOrderId(UUID.randomUUID().toString());
+        OrderStatus orderStatus = new OrderStatus(order, "PROCESS", "order placed succesfully in " + restaurantName);
+        template.convertAndSend(MessagingConfig.EXCHANGE, MessagingConfig.ROUTING_KEY, orderStatus);
+        return "Success !!";
+    }
+}
